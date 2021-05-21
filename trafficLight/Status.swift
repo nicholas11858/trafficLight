49e66a5b236e88ad3e1758d9caee1c822ce06bd8
nicholas11858 <nicholas11858@gmail.com>
2021-05-21 14:50:56 +0200

    Added three view and one button. Changed IOS deployment target, orientation and device family target.

---
+++ trafficLight/Status.swift
@@ -0,0 +1,15 @@
+//
+//  Status.swift
+//  trafficLight
+//
+//  Created by user192060 on 5/21/21.
+//
+
+import Foundation
+
+enum Status {
+    case red
+    case yellow
+    case green
+    case off
+}
--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView! 
+    @IBOutlet weak var greenColorView: UIView!
+    @IBOutlet weak var changeStatusButton: UIButton!
+    
+    private let lightIsOff: CGFloat = 0.3
+    private let lightIsOn: CGFloat = 1.0
+    
+    private var currentStatus: Status = .off
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColorView.alpha = lightIsOff
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
+        
+        redColorView.layer.masksToBounds = true
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        
+        yellowColorView.layer.masksToBounds = true
+        yellowColorView.layer.cornerRadius = redColorView.frame.width / 2
+        
+        greenColorView.layer.masksToBounds = true
+        greenColorView.layer.cornerRadius = redColorView.frame.width / 2    }
+    
+    @IBAction func changeStatus() {
+        switch currentStatus {
+        case .red:
+            greenColorView.alpha = lightIsOff
+            redColorView.alpha = lightIsOn
+            currentStatus = .yellow
+        case .yellow:
+            redColorView.alpha = lightIsOff
+            yellowColorView.alpha = lightIsOn
+            currentStatus = .green
+        case .green:
+            yellowColorView.alpha = lightIsOff
+            greenColorView.alpha = lightIsOn
+            currentStatus = .red
+        case .off:
+            greenColorView.alpha = lightIsOff
+            redColorView.alpha = lightIsOn
+            changeStatusButton.setTitle("Next", for: .normal)
+            currentStatus = .yellow
+        }
     }
-
-
 }
 
